863d630fe82592561da6d2119960b5aa914489c0
Victor Melnyk <victor.melnyk.vm@gmail.com>
2016-03-08 21:27:17 +0200

    inherited google settings

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "google",
+  "globals": {
+    "window": true
+  },
+  "rules": {
+    "one-var": [2, 'always']
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 var foo = bar;
-alert(foo);
